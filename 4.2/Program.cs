2b1a0a53ee05d56c1f9ade0924addaeca8ede18f
Paradoru <LicLexa@yandex.ru>
2021-12-07 23:32:38 +0200

    Добавьте файлы проекта.

---
+++ 4.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] a = new int[100];
+                int n, i;
+                Console.Write("Введите n: ");
+                n = int.Parse(Console.ReadLine());
+                for (i = 0; i < n; i++)
+                {
+                    a[i] = i;
+                    Console.WriteLine(a[i]);
+                }
+            }
+            catch (Exception ex)    
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
